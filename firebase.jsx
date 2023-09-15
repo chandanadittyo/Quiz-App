12b461d8312e550e2c38101997b0a47ad39cfa99
Chandan Debnath <35869474+chandanadittyo@users.noreply.github.com>
2023-09-15 10:18:37 +0600

    fully UI updated

--- firebase.jsx
+++ firebase.jsx
@@ -5,7 +5,7 @@ const app = initializeApp({
   projectId: "quiz-project-9a0e0",
   storageBucket: "quiz-project-9a0e0.appspot.com",
   messagingSenderId: "456327443857",
-  appId: "1:456327443857:web:d5e57b1ef5eb0a880a9630"
-};)
+  appId: "1:456327443857:web:d5e57b1ef5eb0a880a9630",
+});
 
 export default app;
